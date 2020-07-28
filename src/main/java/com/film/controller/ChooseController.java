884309428e3d94385a1d60388ac083220f4ecf88
JF <2636976144@qq.com>
2020-07-28 17:43:11 +0800

    JieJie

--- src/main/java/com/film/controller/ChooseController.java
+++ src/main/java/com/film/controller/ChooseController.java
@@ -1,9 +1,15 @@
 package com.film.controller;
 
+import com.film.pojo.Choose;
+import com.film.pojo.Ordert;
 import com.film.service.IChooseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/choose")
@@ -12,5 +18,41 @@ public class ChooseController {
     @Autowired
     IChooseService chooseService;
 
+    @RequestMapping("/list")
+    @ResponseBody
+    public List<Choose> list(){
+        List<Choose> chooses = chooseService.list();
+        return chooses;
+    }
+
+    @RequestMapping("/edit")
+    @ResponseBody
+    public Choose edit(int id){
+        Choose choose = chooseService.get(id);
+        return choose;
+
+    }
+
+    @RequestMapping("/update")
+    @ResponseBody
+    public ModelAndView update(Choose choose, ModelAndView mav){
+        int i = chooseService.update(choose);
+        mav.setViewName("/");
+        return mav;
+    }
+
+    @RequestMapping("/add")
+    public ModelAndView add(ModelAndView mav, Choose choose){
+        int i = chooseService.add(choose);
+        mav.setViewName("/");
+        return mav;
+    }
+
+    @RequestMapping("/delete")
+    @ResponseBody
+    public int delete(int id){
+        int i = chooseService.delete(id);
+        return i;
+    }
 
 }
