1c8b55e65d3fe12e720a0ead2b299d9442963a34
ZhangShao <2636976144@qq.com>
2020-07-28 16:47:32 +0800

    ZhangShao

---
+++ src/main/java/com/film/controller/hellodemo.java
@@ -0,0 +1,20 @@
+package com.film.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class hellodemo {
+//    @RequestMapping("/hello")
+//    public String hello(){
+//        System.out.println("aa");
+//        return "index";
+//    }
+    @RequestMapping("/admin/index.html")
+    public ModelAndView update(ModelAndView mav){
+        System.out.println("update");
+        mav.setViewName("admin/index");
+        return mav;
+    }
+}
