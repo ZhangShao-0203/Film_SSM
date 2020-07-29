2016b38e3d9a6cba73fb566fbfcc10bc0ed39497
ZhangShao <2636976144@qq.com>
2020-07-29 09:53:37 +0800

    ZhangShao

--- src/main/java/com/film/controller/ActorController.java
+++ src/main/java/com/film/controller/ActorController.java
@@ -6,10 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
 import java.security.Permission;
 import java.util.List;
+import java.util.UUID;
 
 @Controller
 @RequestMapping("/actor")
@@ -42,7 +45,11 @@ public class ActorController {
     }
 
     @RequestMapping("/add")
-    public ModelAndView add(ModelAndView mav, Actor actor){
+    public ModelAndView add(ModelAndView mav, Actor actor, MultipartFile doc, HttpServletRequest request){
+        String originalFilename = doc.getOriginalFilename();
+        String substring = originalFilename.substring(originalFilename.lastIndexOf("."));
+        String s = UUID.randomUUID().toString();
+        request.getRealPath("/")+"docs/"+s+doc+substring;
         int i = actorService.add(actor);
         mav.setViewName("/");
         return mav;
--- src/main/java/com/film/controller/hellodemo.java
+++ src/main/java/com/film/controller/hellodemo.java
@@ -5,27 +5,57 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping("/admin")
 public class hellodemo {
-//    @RequestMapping("/hello")
-//    public String hello(){
-//        System.out.println("aa");
-//        return "index";
-//    }
-    @RequestMapping("/admin/index.html")
+    @RequestMapping("/index.html")
     public ModelAndView update(ModelAndView mav){
         System.out.println("update");
         mav.setViewName("admin/index");
         return mav;
     }
-    @RequestMapping("/admin/actor.html")
+    @RequestMapping("/actor.html")
     public String actor(){
         System.out.println("actor");
         return "admin/actor";
     }
-
-    @RequestMapping("/admin/listVip.html")
-    public String listVip(){
-        System.out.println("listVip");
-        return "admin/listVip";
+    @RequestMapping("/cinema.html")
+    public String cinema(){
+        System.out.println("cinema");
+        return "admin/cinema";
+    }
+    @RequestMapping("/choose.html")
+    public String choose(){
+        System.out.println("choose");
+        return "admin/choose";
+    }
+    @RequestMapping("/movie.html")
+    public String movie(){
+        System.out.println("movie");
+        return "admin/movie";
+    }
+    @RequestMapping("/ordert.html")
+    public String ordert(){
+        System.out.println("ordert");
+        return "admin/ordert";
+    }
+    @RequestMapping("/screen.html")
+    public String screen(){
+        System.out.println("screen");
+        return "admin/screen";
+    }
+    @RequestMapping("/update.html")
+    public String update(){
+        System.out.println("update");
+        return "admin/update";
+    }
+    @RequestMapping("/video.html")
+    public String video(){
+        System.out.println("video");
+        return "admin/video";
+    }
+    @RequestMapping("/vip.html")
+    public String vip(){
+        System.out.println("vip");
+        return "admin/vip";
     }
 }
