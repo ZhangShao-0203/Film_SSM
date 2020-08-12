841f381c6e0ee0fa404dadfddefba154055805e4
ZhangShao <2636976144@qq.com>
2020-08-12 18:33:09 +0800

    ZhangShao

--- src/main/java/com/film/SpringBootApp.java
+++ src/main/java/com/film/SpringBootApp.java
@@ -1,13 +1,12 @@
 package com.film;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-
 @SpringBootApplication
-public class SpringBootApp extends SpringBootServletInitializer{
+public class SpringBootApp extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(SpringBootApp.class,args);
     }
